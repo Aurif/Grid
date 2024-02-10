6fe72c6bcb8272693de454853ca87e5ea2f88221
Filmos <filmosyt@gmail.com>
2024-02-10 18:06:44 +0100

    Rework - initial commit

---
+++ grid/vite.config.ts
@@ -0,0 +1,16 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { viteSingleFile } from "vite-plugin-singlefile"
+
+export default defineConfig({
+  plugins: [
+    [vue(), viteSingleFile()],
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
